f3c617c5ffba69bf306c9d8a3ac0eeffaf092649
dzowera <idzowera@byupathway.edu>
2025-09-07 14:31:09 +0200

    fixed date issues

--- scripts/dates.js
+++ scripts/dates.js
@@ -1,6 +1,6 @@
 // i get the last date modified - the document acts like an object with lastmodified as a property
 const lastDateModified = new Date(document.lastModified);
 // I now need to format the date to make it look nice and simple
-const formattedDate = `${lastDateModified.getFullYear()} / 0${lastDateModified.getMonth() + 1} / 0${lastDateModified.getDay()}`;
+const formattedDate = `${lastDateModified.getFullYear()} / 0${lastDateModified.getMonth() + 1} / 0${lastDateModified.getDate()} - ${lastDateModified.getHours()}:${lastDateModified.getHours()}`;
 
 document.querySelector('.last-modified').innerHTML = formattedDate;
