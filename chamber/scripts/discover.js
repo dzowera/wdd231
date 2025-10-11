d0d5434b902529edd7df5fa26b41b089f5641a4d
dzowera <idzowera@byupathway.edu>
2025-10-11 08:33:30 +0200

    added a picture proper format

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,5 +1,16 @@
 import { lilongweAreas } from "./places.js";
 
-lilongweAreas.forEach(area =>{
-  console.log(area);
-})
+let html = '';
+lilongweAreas.forEach(area => {
+  html += `
+    <div class="place-card">
+      <img src="${area.imageUrl}" alt="${area.location}">
+      <h2>${area.name}</h2>
+      <p class="location">${area.location}</p>
+      <p class="desc">${area.description}</p>
+    </div>
+  `;
+});
+
+const container = document.querySelector('.places-container'); // Use a wrapper for all cards
+container.innerHTML = html;
